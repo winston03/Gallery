e4e9d7a2f4936414a83da453d8987e96439e0bf8
Winston Tamin <winstontamin@gmail.com>
2024-12-22 22:01:45 +0700

    third commit

--- src/components/kartu/Card.jsx
+++ src/components/kartu/Card.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Card = (props) => {
     const { img, date } = props
+    const [isHovered, setisHovered] = useState(false)
+    const toggleHover = () => {
+        setisHovered(!isHovered)
+    }
     return (
         <div className="group relative overflow-hidden cursor-pointer shadow-2xl flex items-center justify-center">
             <img src={img} alt="" className="cursor-pointer" />
-            <div className="absolute top-0 left-0 bg-gradient-to-t transition-all ease-in-out duration-500 group-hover:translate-y-0 translate-y-full flex justify-center items-center w-full h-full from-black/80 to-transparent">
+            <div onClick={() => toggleHover()} className={`${isHovered ? 'translate-y-0' : 'translate-y-full'} absolute top-0 left-0 bg-gradient-to-t transition-all ease-in-out duration-500 group-hover:translate-y-0 translate-y-full flex justify-center items-center w-full h-full from-black/80 to-transparent`}>
                 <p className="text-white font-whisper text-2xl">{date}</p>
             </div>
         </div>
